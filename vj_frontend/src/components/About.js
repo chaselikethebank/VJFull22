ccc3477579582865e3d52c1a0b20e01b20c847aa
Chase <chasedemaster.tech@gmail.com>
2022-08-22 17:10:10 -0500

    about copy

--- vj_frontend/src/components/About.js
+++ vj_frontend/src/components/About.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Box } from '@mui/system';
+import Typography from '@mui/material/Typography';
+
 
 function About() {
   return (
     <div className="App">
-            <Box m={1} pt={3}></Box>
+            <Box m={1} pt={3} align="left" variant="h3">
+<Typography variant="h3">
+            #veryjazzed is a boutique music rights management / record label. We have been working with inspired creators in the music space since 2013. Our roster is special, our artists are special, our label is special. We understand who we are serving and to what end. We are fixed on quality, people, and growth. We have our own language. We take small steps, but we take them everyday. Our network is interdependent. We share knowledge and resources freely and without conditionals. We leverage the finite against our unending ability to create new and inspired works. We pride ourselves in recognizing strengths and bridging blindspots. <p></p>
 
-      About route
+We have three sides We are public facing: promoting works, tours, and success stories. We are artist facing: managing the needs, visions, and admin tasks for our artists. We are industry facing: building an infrastructure by partnering with some of the most earnest and integral companies in the space.  <p></p>
+
+We are always looking to work with new artists. Unlike most labels, a project/brand has to be at a certain level for it to make sense. We are more interested in finding people who are inspired and easy to work with. If those two things are clear, we are in. Whatever isn’t there, that’s what we are good at. We believe there are some basic aspects to a successful release: great recordings, brand clarity, storytelling, strong visual assets, manageable marketing strategies, leverageable live performances, earnest sm, high-value merch, and archiving for the future. When a project gets on radar, we do our best to see it for what it is, what it could be, and help facilitate the delta between the two. Sometimes it looks like acquiring a publicist or linking a mixing engineer with an artist, while other times it might be pitching larger acts for tour support in their regional area or buying a projector and screen for a projects said tour support slot. Above all, we are super flexible and always try to get back to first principles when deciding what the next steps might be “from here”. <p></p>
+
+If you are a maker, an admin, an agent, a fan and you appreciate what we are doing and want to be be on radar moving forward. We are here: veryjazzedATgmailDOTcom<p></p>
+</Typography>
+</Box>
     </div>
   );
 }
