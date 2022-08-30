6d0b2a9edca92d89de39cc1fce75d893b1e5d82c
Chase <chasedemaster.tech@gmail.com>
2022-08-30 09:38:48 -0500

    emojis in about

--- vj_frontend/src/components/Home.js
+++ vj_frontend/src/components/Home.js
@@ -3,14 +3,13 @@ import React from "react";
 import { Box } from "@mui/system";
 import logo from "./shortEditsFromHandyCamVJ.mp4";
 // import About from "./components/About";
-import Typography from '@mui/material/Typography';
-
+import Typography from "@mui/material/Typography";
 
 function Home() {
   return (
     <div className="App">
       <Box m={1} pt={3}>
-      <video width="2400" autoPlay loop muted>
+        <video width="2400" autoPlay loop muted>
           <source
             src={logo}
             alt="Logo"
@@ -21,21 +20,57 @@ function Home() {
             }}
           />
         </video>
-       
       </Box>
       <Box m={1} pt={3} align="left" variant="h3">
-<Typography variant="h3" sx={{ fontFamily: 'Arial', textTransform: 'uppercase', fontWeight: 'bold', fontOpticalSizing: 'auto'  }}>
-            #veryjazzed is a boutique music rights management / record label. Gradient since 2013. Our roster is special, our artists are special, our label is special. We understand who we are serving and to what end. We are fixed on quality, people, and growth. We have our own language. We take small steps early and often. Our network is interdependent. We share knowledge and resources freely and without conditionals. We leverage the finite against our unending ability to create new and inspired works. We pride ourselves in recognizing strengths and bridging blindspots. <p></p>
-
-We have three sides. We are public facing: promoting works, tours, and success stories. We are artist facing: managing the needs, visions, and admin tasks for our artists. We are industry facing: building an infrastructure by partnering with some of the most earnest and integral companies in the space. We are always connecting with new people in each of these domains. Our model is to find inspired people who are easy to engage with and build difficult things with them.  <p></p>
-
- We believe there are some basic aspects to a successful release: great recordings, brand clarity, storytelling, strong visual assets, manageable marketing strategies, leverageable live performances, earnest sm, high-value merch, and archiving for the future. When a project gets on radar, we do our best to see it for what it is, what it could be, and help facilitate the delta between the two. Sometimes it looks like acquiring a publicist or linking a mixing engineer with an artist, and other times it might be pitching larger acts for tour support in their regional area or buying a projector and screen for said tour support slot. We are super flexible, always reverting to first principles when deciding what is our next and best step from here. <p></p>
-
-If you are a listener, a maker, or a builder and you resonate with what we are doing, we should know about each other. We would love to hear what you are excited about and how we might be a valuable resource. Reach out to us here: veryjazzedATgmailDOTcom<p></p>
-🌊 , <br></br>
-Chase
-</Typography>
-</Box>
+        <Typography
+          variant="h3"
+          sx={{
+            fontFamily: "Arial",
+            textTransform: "uppercase",
+            fontWeight: "bold",
+            fontOpticalSizing: "auto",
+          }}
+        >🥀<p></p>
+          #veryjazzed is a boutique music rights management / record label.
+          Gradient since 2013. Our roster is special, our artists are special,
+          our label is special. We understand who we are serving and to what
+          end. We are fixed on quality, people, and growth. We have our own
+          language. We take small steps early and often. Our network is
+          interdependent. We share knowledge and resources freely and without
+          conditionals. We leverage the finite against our unending ability to
+          create new and inspired works. We pride ourselves in recognizing
+          strengths and bridging blindspots. <p></p>
+          🗻<p></p>
+          We have three sides. We are public facing: promoting works, tours, and
+          success stories. We are artist facing: managing the needs, visions,
+          and admin tasks for our artists. We are industry facing: building an
+          infrastructure by partnering with some of the most earnest and
+          integral companies in the space. We are always connecting with new
+          people in each of these domains. Our model is to find inspired people
+          who are easy to engage with and build difficult things with them.{" "}
+          <p></p>
+          ☸<p></p>
+          We believe there are some basic aspects to a successful release: great
+          recordings, brand clarity, storytelling, strong visual assets,
+          manageable marketing strategies, leverageable live performances,
+          earnest sm, high-value merch, and archiving for the future. When a
+          project gets on radar, we do our best to see it for what it is, what
+          it could be, and help facilitate the delta between the two. Sometimes
+          it looks like acquiring a publicist or linking a mixing engineer with
+          an artist, and other times it might be pitching larger acts for tour
+          support in their regional area or buying a projector and screen for
+          said tour support slot. We are super flexible, always reverting to
+          first principles when deciding what is our next and best step from
+          here. <p></p>
+          🔭<p></p>
+          If you are a listener, a maker, or a builder and you resonate with
+          what we are doing, we should know about each other. We would love to
+          hear what you are excited about and how we might be a valuable
+          resource. Reach out to us here: veryjazzedATgmailDOTcom<p></p>
+          🌊 , <br></br>
+          Chase
+        </Typography>
+      </Box>
     </div>
   );
 }
